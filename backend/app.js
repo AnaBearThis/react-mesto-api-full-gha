75ac5f3799d0dfebfca9ed99b8907b8c0c436a10
Ana Bear <elukova2010@yandex.ru>
2023-07-02 23:04:04 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   if (allowedCors.includes(origin)) {
+    console.log(origin);
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
